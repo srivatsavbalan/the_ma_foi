ffe5a53d9717d97aca35e4ad33f4db06a3796c85
praveen vignesh <praveenvignesh31@gmail.com>
2016-11-13 02:56:28 +0530

    Added api for skills

--- pretium-backend/config/routes.rb
+++ pretium-backend/config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
   resources :students
   resources :colleges
   post '/users/login', to: 'users#login'
+  get 'skills_categories', to: 'skills#skills_categories'
+  get 'skills_sub_categories', to: 'skills#skills_sub_categories'
   # devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
