f6bae015d56fb5040ae4e1c20ea96b16863cf002
mukilarasan.r <mukilarasan.r@zohocorp.com>
2016-11-13 15:34:05 +0530

    rating was added under detail view.

---
+++ pretium-frontend/app/controllers/student/view.js
@@ -0,0 +1,44 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+	init: function () {
+	},
+	students : function() {
+        var students = this.get('store').findAll('student');
+        return students;
+    }.property('model'),
+    skills : function() {
+        var skills = this.get('store').findAll('skill');
+        return skills;
+    }.property('model'),
+    actions: {
+        saveRating: function(){
+            let student = $("#student_id").val();
+            let skills = $("#skills").val();
+            let rating = $("#rating").val();
+            let review = $("#review").val();
+            if(rating>10){
+                alert("Sorry, you can you more than 10");
+                return;
+            }
+
+            let param={};
+            param["student_id"]=student;
+            param["skill_id"]=skills;
+            param["teacher_id"]=window.localStorage.getItem("userid");
+            param["rating"]=rating;
+            param["review"]=review;
+
+
+            var post = this.store.createRecord('rating', param);
+            var thisObj = this;
+
+            post.save().then((savedRecord) => {
+                alert("Record saved..");
+            }).catch((xhr) => {
+                console.log("error occurred and res status ", xhr);
+                thisObj.transitionToRoute('/');
+            });
+        }
+    }
+});
