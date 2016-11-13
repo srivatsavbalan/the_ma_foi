bb66f127f0cd50d845623954c8540d25bf81e055
praveen vignesh <praveenvignesh31@gmail.com>
2016-11-13 10:03:20 +0530

    added metrics API for landing page, student list view for teachers API

--- pretium-backend/app/controllers/application_controller.rb
+++ pretium-backend/app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  # before_action :validate_login
+  before_action :validate_login
   protect_from_forgery
 
   def validate_login
@@ -15,8 +15,8 @@ class ApplicationController < ActionController::Base
   end
 
   def current_user
-    # return unless session[:user_id]
-    session[:user_id] = 1
+    return unless session[:user_id]
+    # session[:user_id] = 1
     @current_user ||= User.find(session[:user_id])
   end
 end
--- pretium-backend/app/models/rating.rb
+++ pretium-backend/app/models/rating.rb
@@ -2,5 +2,8 @@ class Rating < ApplicationRecord
   belongs_to :student
   belongs_to :teacher
   belongs_to :skill
+  def self.threshold_to_display
+    30
+  end
   # belongs_to :college, through: :teachers
 end
