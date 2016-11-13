38ee1fa4f34956af48e6062418a28e78b561996d
praveen vignesh <praveenvignesh31@gmail.com>
2016-11-13 12:18:28 +0530

    Added landing screen, login screen for student app

--- pretium_student_hybrid_app/index.ios.js
+++ pretium_student_hybrid_app/index.ios.js
@@ -5,29 +5,52 @@
  */
 
 import React, { Component } from 'react';
+import Login from "./login"
+import Profile from "./profile"
+import SplashScreen from "./splashscreeen"
 import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  AsyncStorage,
+  Alert
 } from 'react-native';
 
 export default class pretium_student_hybrid_app extends Component {
+  constructor(props) {
+    super(props);
+  
+    this.state = {
+      loading: true,
+      authorized: false
+    };
+    var thisObj = this;
+    // AsyncStorage.getItem('@authorization:auth_token').then((auth_token) => {
+    //   thisObj.setState({"loading": false})
+    //   Alert.alert("Async Success")
+    //   if ( auth_token ) {
+    //     Alert.alert("No Auth")
+    //     thisObj.setState({"authorized": true})
+    //   } else {
+    //     Alert.alert("Auth")
+    //     thisObj.setState({"authorized": false})
+    //   }
+    //  }).catch(() => {
+    //    thisObj.setState({"loading": false})
+    //    thisObj.setState({"authorized": false})
+    //  })
+
+  }
   render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
+    if ( this.state.loading ) {
+      return (<SplashScreen />);
+    } else {
+      if ( !this.state.authorized ) {
+        return (<Login />);
+      }
+      return (<Profile />);
+    }
   }
 }
 
---
+++ pretium_student_hybrid_app/profile.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  Image
+} from 'react-native';
+
+export default class Profile extends Component {
+  constructor(props) {
+    super(props);
+
+  }
+  render() {
+    return (
+      <View>
+        
+      </View>
+    )
+  }
+}
+
+AppRegistry.registerComponent('Profile', () => Profile);
