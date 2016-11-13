10633e9bf03b0e55dbe6b49c89619ba121669e4b
Sakthi-Gnanam <sakthignanam@outlook.com>
2016-11-13 12:59:19 +0530

    student view/edit/save

--- pretium-frontend/app/routes/student/index.js
+++ pretium-frontend/app/routes/student/index.js
@@ -2,7 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
 	model() {
-			let id = window.localStorage.getItem("userid");
-    return this.get('store').findRecord('student',id);
+		return this.get('store').findAll('teacher');
   	}
 });
